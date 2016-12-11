b7a999baaa53bdd74abdb1861c08e0e873495073
Waylon Flinn <waylonflinn@gmail.com>
2016-12-11 15:20:44 -0600

    first well formed test

--- test/neuron.js
+++ test/neuron.js
@@ -3,6 +3,8 @@ var Neuron = require('../lib/neuron.js'),
 	weblas = require('weblas'),
 	tape = require('tape');
 
+weblas.test = require('weblas/lib/test');
+
 var RTOL = 1e-05,
 	ATOL = 1e-07;
 
@@ -11,15 +13,15 @@ tape("neuron", function(t){
 
 	var weights = new Float32Array([1.0, 1.0, 1.0, 1.0]),
 		bias = new Float32Array([3.5, 3.5, 3.5, 3.5]),
-		dropout = 0.0;
+		dropout = 1.0;
 
-	var expected = new Float32Array([]);
+	var input = new Float32Array([1.0]);
+	var expected = new Float32Array([4.5, 4.5, 4.5, 4.5]);
 
 	var neuron = new Neuron(weights, bias, dropout);
 
 	try{
-		result = neuron.forward(input);
-
+		result = neuron.forward(input).transfer();
 	}
 	catch(ex){
 		t.error(ex);
